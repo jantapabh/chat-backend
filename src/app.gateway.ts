775e008f8653a581dfb67e8dbab777b812c939d9
jantapabh <jantapa2407@gmail.com>
2022-06-16 17:31:37 +0700

    initial project

---
+++ src/app.gateway.ts
@@ -0,0 +1,38 @@
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, payload: string): void {
+    console.log("payload backend : ",payload);
+    
+    this.server.emit('msgToServer', payload, client.id);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connection: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnection: ${client.id}`);
+  }
+}
